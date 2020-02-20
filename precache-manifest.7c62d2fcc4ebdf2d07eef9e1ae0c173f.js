2965f4e7f5f900cb1346378292b1d87055b93f30
dmb-2222 <dmb-2222@yandex.ru>
2020-02-20 17:37:29 +0200

    Updates

---
+++ precache-manifest.7c62d2fcc4ebdf2d07eef9e1ae0c173f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d129cf86c59f7c4bdcac7c0f960cb3",
+    "url": "/binary-studio-test-task/index.html"
+  },
+  {
+    "revision": "c92c4f827cf0be30fd67",
+    "url": "/binary-studio-test-task/static/css/main.07daf812.chunk.css"
+  },
+  {
+    "revision": "0891ff67f2c47c79edde",
+    "url": "/binary-studio-test-task/static/js/2.a1c3c62e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/binary-studio-test-task/static/js/2.a1c3c62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92c4f827cf0be30fd67",
+    "url": "/binary-studio-test-task/static/js/main.b30a389c.chunk.js"
+  },
+  {
+    "revision": "17a857950c5fefa29cf8",
+    "url": "/binary-studio-test-task/static/js/runtime-main.9875f2fe.js"
+  }
+]);
